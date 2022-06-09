fddce9a3018f1618bb9d1181119a955eab50fc0f
yinlinghunjiang <1581647952@qq.com>
2022-06-09 21:01:22 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Simple-Tranfur-Bot
-A simple tranfur QQBot powered by miraicle
+A simple tranfur QQBot powered by Tail Api
+
+一个简单，开箱即用的吸毛Bot
+
+## Quick start: ##
+<br>
+  For Windows 
+
+    Edit /config/main.conf 
+    cd ../
+    pip install -r requirements.txt
+    python tranfur.py
+	python bot.py(Don't stop running tranfur.py)
+
+  For Linux
+
+    nano /config/main.conf 
+    cd ../
+    pip install -r requirements.txt
+    screen -r botpy
+    python tranfur.py
+    screen -r transfur
+    python bot.py
+
+Enjoy it~
+
+## 快速上手 ##
+<br>
+   对于Windows系统
+
+    编辑 /config/main.conf 
+    cd ../
+    pip install -r requirements.txt
+    python tranfur.py
+	python bot.py(不要关闭tranfur.py的黑框框)     
+   对于Linux系统
+
+    nano /config/main.conf 
+    cd ../
+    pip install -r requirements.txt
+    screen -r botpy
+    python tranfur.py
+    screen -r transfur
+    python bot.py
+
+由于本人还很菜，所以有什么代码问题欢迎Issue和pr
+
+That's it
---
+++ bot.py
@@ -0,0 +1,67 @@
+import miraicle
+import requests
+import json
+from configparser import ConfigParser# 读取配置, 加载配置项
+def getport():
+    config = ConfigParser()
+    config.read('./config/main.conf', encoding='UTF-8')
+    return str(config['flaskconf']['port'])
+def getFursuitRand():
+    r =  requests.get("http://127.0.0.1:"+getport()+"/getFursuitRand")
+    json_str = json.loads(r.text)
+    return json_str
+def getFursuitByName(name):
+    r =  requests.get("http://127.0.0.1:"+getport()+"/getFursuitByName/"+name)
+    json_str = json.loads(r.text)
+    return json_str
+def getFursuitByID(furid):
+    r =  requests.get("http://127.0.0.1"+getport()+"/getFursuitByID/"+str(furid))
+    json_str = json.loads(r.text)
+    return json_str
+@miraicle.Mirai.receiver('GroupMessage')
+def hello_to_group(bot: miraicle.Mirai, msg: miraicle.GroupMessage):
+        #if msg.plain in [str("来只 "+re.fullmatch('(?<=来只 ).*$', msg.plain)[0])]:
+    try:
+        if msg.plain in ["来只 "+msg.plain.split(" ")[1]]:
+                # r =  requests.get("http://192.168.1.12:9000/getFursuitByName/"+str(re.fullmatch('(?<=来只 ).*$', msg.plain)[0]))
+                # json_str = json.loads(r.text)
+            json_raw = getFursuitByName(int(msg.plain.split(" ")[1]))
+            furid = json_raw['data']['id']
+            name=json_raw['data']['name']
+            thumb=json_raw['data']['url']
+            bot.send_group_msg(group=msg.group, msg=[miraicle.Plain('--- 每日吸毛 Bot ---\n今天你吸毛了嘛？\nFurID:'+str(furid)+'\n毛毛名字：'+name+'\n搜索方法：模糊\n'),miraicle.Image(url=thumb)])
+        if msg.plain in ["查毛图 "+msg.plain.split(" ")[1]]:
+            json_raw = getFursuitByID(msg.plain.split(" ")[1])
+            furid = json_raw['data']['id']
+            name=json_raw['data']['name']
+            thumb=json_raw['data']['url']
+            bot.send_group_msg(group=msg.group, msg=[miraicle.Plain('--- 每日吸毛 Bot ---\n今天你吸毛了嘛？\nFurID:'+str(furid)+'\n毛毛名字：'+name+'\n搜索方法：精确\n'),miraicle.Image(url=thumb)])
+    except IndexError as e:
+        if msg.plain in ['help', '.help','/help']:
+            bot.send_group_msg(group=msg.group, msg=[miraicle.At(qq=msg.sender),miraicle.Plain(' 帮助文档:https://www.uwpg.xyz/docs')])
+        if msg.plain in ['来只毛','.transfur']:
+            json_raw=getFursuitRand()
+            furid = json_raw['data']['id']
+            name=json_raw['data']['name']
+            thumb=json_raw['data']['url']
+            bot.send_group_msg(group=msg.group, msg=[miraicle.Plain('--- 每日吸毛 Bot ---\n今天你吸毛了嘛？\nFurID:'+str(furid)+'\n毛毛名字：'+name+'\n搜索方法：全局随机\n'),miraicle.Image(url=thumb)])
+    except KeyError as er:
+        helper = ConfigParser()
+        helper.read('./config/main.conf', encoding='UTF-8')
+        bot.send_group_msg(group=msg.group, msg=[miraicle.Plain(helper['mirai']['furry_not_found'])])
+
+@miraicle.Mirai.receiver('FriendMessage')
+def hello_to_friend(bot: miraicle.Mirai, msg: miraicle.FriendMessage):
+    if msg.plain in ['help', '.help','/help']:
+        helper = ConfigParser()
+        helper.read('./config/main.conf', encoding='UTF-8')
+        bot.send_friend_msg(qq=msg.sender, msg=helper['mirai']['help'])
+        
+miraiconf = ConfigParser()
+miraiconf.read('./config/main.conf', encoding='UTF-8')
+qq = miraiconf['mirai']['qq']       # 你登录的机器人 QQ 号
+verify_key = miraiconf['mirai']['verifykey']     # 你在 setting.yml 中设置的 verifyKey
+port = miraiconf['mirai']['port']                 # 你在 setting.yml 中设置的 port (http)
+
+bot = miraicle.Mirai(qq=qq, verify_key=verify_key, port=port)
+bot.run()
